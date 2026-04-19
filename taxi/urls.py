35f2f5d7f07c358410cb51d1fa5a1a5469e1fdb2
Farlinder <sashakost2006e@gmail.com>
2026-04-19 19:18:41 +0200

    Solving

--- taxi/urls.py
+++ taxi/urls.py
@@ -46,11 +46,9 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path(
-        "cars/<int:pk>/toggle-assign/",
+    path("cars/<int:pk>/toggle-assign/",
          toggle_assign_to_car,
-         name="car-toggle-assign"
-    ),
+         name="car-toggle-assign"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/",
